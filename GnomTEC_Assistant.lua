79cf9a44a882c1cf5303675fb643bc6f7bf7789a
GnomTEC <info@gnomtec.de>
2019-12-22 17:59:47 +0100

    * Updated TOC for WoW patch 1.13.3 (Classic)

--- GnomTEC_Assistant.lua
+++ GnomTEC_Assistant.lua
@@ -1,6 +1,6 @@
 -- **********************************************************************
 -- GnomTEC Assistant
--- Version: 8.2.5.3
+-- Version: 8.2.5.4
 -- Author: Peter Jack
 -- URL: http://www.gnomtec.de/
 -- **********************************************************************
@@ -28,8 +28,8 @@ local L = LibStub("AceLocale-3.0"):GetLocale("GnomTEC_Assistant")
 local addonInfo = {
 	["Name"] = "GnomTEC Assistant",
 	["Description"] = "Addon which will assist all GnomTEC addons in future with centralized templates and functionality.",	
-	["Version"] = "8.2.5.3",
-	["Date"] = "2019-11-09",
+	["Version"] = "8.2.5.4",
+	["Date"] = "2019-12-22",
 	["Author"] = "Peter Jack",
 	["Email"] = "info@gnomtec.de",
 	["Website"] = "http://www.gnomtec.de/",
