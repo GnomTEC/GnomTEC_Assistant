249fe5264be0b47dabadf3b4958f66e31ae66168
Effie Flammfix <github@gnomtec.de>
2023-09-13 17:54:44 +0200

    * Updated TOC for WoW patch 10.1.7

--- GnomTEC_Assistant.lua
+++ GnomTEC_Assistant.lua
@@ -1,6 +1,6 @@
 -- **********************************************************************
 -- GnomTEC Assistant
--- Version: 10.1.5.5
+-- Version: 10.1.7.5
 -- Author: Peter Jack
 -- URL: http://www.gnomtec.de/
 -- **********************************************************************
@@ -28,8 +28,8 @@ local L = LibStub("AceLocale-3.0"):GetLocale("GnomTEC_Assistant")
 local addonInfo = {
 	["Name"] = "GnomTEC Assistant",
 	["Description"] = "Addon which will assist all GnomTEC addons in future with centralized templates and functionality.",	
-	["Version"] = "10.1.5.5",
-	["Date"] = "2023-07-12",
+	["Version"] = "10.1.7.5",
+	["Date"] = "2023-09-13",
 	["Author"] = "Peter Jack",
 	["Email"] = "info@gnomtec.de",
 	["Website"] = "http://www.gnomtec.de/",
